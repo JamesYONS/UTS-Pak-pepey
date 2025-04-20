fa6877c49a22748459ed6309553e0411d4852807
JamesYONS <triyonosimanjuntak60@gmail.com>
2025-04-20 22:45:26 +0700

    SISTEM OPERASI

---
+++ AutoGen.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+echo "Membuat Struktur Folder"
+mkdir BackEnd FrontEnd
+
+echo "Membuat Struktur Folder dan File 1"
+touch FrontEnd/{index.html,style.css,main.js}
+
+echo "Membuat Struktur Folder dan File 2"
+touch BackEnd/{index.html,style.html,dashboard.html,login.html}
+
+echo ".... Selesai"
